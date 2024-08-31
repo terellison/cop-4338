de37cc97ae187bc62c076c2c9d2d7629c744dd37
terellison <ellisonterry4@gmail.com>
2024-08-31 14:59:15 -0400

    added main.h

---
+++ assignment-1/main.h
@@ -0,0 +1,7 @@
+int main(void);
+void singleton(const char* str, const unsigned int len);
+void arithmetic(const char* str, const unsigned int len);
+void reverse_arithmetic(const char* str, const unsigned int len);
+void tripartite(const char* str, const unsigned int len);
+void bipartite(const char* str, const unsigned int len);
+void palindrome(const char* str, const unsigned int len);
