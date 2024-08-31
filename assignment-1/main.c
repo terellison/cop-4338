8cfab973af918be84b715869d7ff3cc7ddecaf6b
terellison <ellisonterry4@gmail.com>
2024-08-31 14:59:43 -0400

    finished singleton implementation

--- assignment-1/main.c
+++ assignment-1/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include "main.h"
 #include "helpers.h"
 
 int main(void)
@@ -8,12 +10,37 @@ int main(void)
 
     while(1)
     {
-        scanf("%s", line);
-        char reversed[LENGTH];
-        reverse_str(line, LENGTH, reversed);
-        printf("%s", reversed);
+        fgets(line, LENGTH, stdin);
+        unsigned int len = strlen(line) - 1;
+
+        if(len <= 0)
+            continue;
+
+        singleton(line, len);
+        // arithmetic(line, len);
+        // tripartite(line, len);
+        // bipartite(line, len);
+        // palindrome(line, len);
+
+        printf("%c", '\n');
     }
 
-
     return 0;
+}
+
+void singleton(const char* str, const unsigned int len)
+{
+    int isSingleton = 1;
+    if(len > 1)
+    {
+        char c = str[0];
+
+        for(unsigned int i = 0; i < len && isSingleton == 1; ++i)
+        {
+            isSingleton = c == str[i];
+        }
+    }
+
+    if(isSingleton)
+        printf("%s", "singleton\n");
 }
